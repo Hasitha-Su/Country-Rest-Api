888f947cb99587f605a77aea3391ed4dc30a347a
Hasitha_IT17132138 <it17132138@my.sliit.lk>
2020-12-04 14:31:13 +0530

    country rest api

--- springboot-country-crud/springboot-country-crud/src/main/java/com/country/controller/CountryController.java
+++ springboot-country-crud/springboot-country-crud/src/main/java/com/country/controller/CountryController.java
@@ -3,10 +3,12 @@ package com.country.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,24 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.country.model.Country;
 import com.country.service.CountryService;
 
-
-//creating RestController
 @RestController
 @RequestMapping("/rest/v2")
-
 public class CountryController {
 	
-//auto wired the CountryService class	
+	//auto wired the CountryService class	
 	@Autowired
 	CountryService countryService;
 
-//creating a get mapping that retrieves all the countries details 
+	//creating a get mapping that retrieves all the countries details 
 	@GetMapping("/countries")
 	private List<Country> getAllCountries() {
 		return countryService.getAllCountries();
 	}
 
-//creating a get mapping that retrieves the detail of a specific country by id
+	//creating a get mapping that retrieves the detail of a specific country by id
 	@GetMapping("/country/{id}")
 	private Country getCountry(@PathVariable("id") int id) {
 		return countryService.getCountryById(id);
@@ -42,11 +41,10 @@ public class CountryController {
 		countryService.delete(id);
 	}
 
-//creating post mapping that post the country details 
+	//creating post mapping that post the country details 
 	@PostMapping("/country")
 	private int saveCountry(@RequestBody Country country) {
 		countryService.saveOrUpdate(country);
 		return country.getId();
 	}
-
 }
--- springboot-country-crud/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-
-DROP TABLE IF EXISTS Country;
-  
-CREATE TABLE Country (
-  id INT NOT NULL AUTO_INCREMENT,
-  countryName VARCHAR(250) NOT NULL,
-  PRIMARY KEY  (id)
-);
